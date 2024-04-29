a18968a0f52b70d1c5ff8e9545ff0ae8fc4e9d18
Caio Costa <caiocostaee@gmail.com>
2024-04-29 14:41:14 -0300

    first

--- App.js
+++ App.js
@@ -1,11 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as TaskManager from 'expo-task-manager';
+import * as Location from 'expo-location';
+
+const BACKGROUND_FETCH_TASK = 'background-fetch-task';
+
+TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
+
+  try {
+
+    const now = Date.now();
+
+    // Tenta obter a localização atual do usuário
+    const location = await getLocationAsync();
+    console.log(`Got background fetch call at date: ${new Date(now).toISOString()}`);
+    console.log(`Current location: Latitude ${location?.coords?.latitude}, Longitude ${location?.coords?.longitude}`);
+
+    // await request();
+    return BackgroundFetch.BackgroundFetchResult.NewData;
+
+  } catch (error) {
+    console.log({ errorOnDefineTask: error })
+  }
+});
+
+async function registerBackgroundFetchAsync() {
+  // Primeiro, assegure-se de ter permissão para acessar a localização
+  const { status } = await Location.requestForegroundPermissionsAsync();
+  if (status === 'granted') {
+    return BackgroundFetch.registerTaskAsync(BACKGROUND_FETCH_TASK, {
+      minimumInterval: 60 * 1, // Intervalo mínimo de 1 minuto
+      stopOnTerminate: false, // Continua executando mesmo após o app fechar
+      startOnBoot: true, // Inicia automaticamente quando o dispositivo é ligado
+    });
+  } else {
+    console.log('Location permission not granted');
+  }
+}
+
+async function getLocationAsync() {
+  try {
+    let location = await Location.getCurrentPositionAsync({});
+    console.log({ location })
+    return location;
+  } catch (error) {
+    console.error("Couldn't get location", error);
+    return null;
+  }
+}
+
+const request = async () => {
+  try {
+    var response = await fetch('http://192.168.15.7:3000/livros');
+    response = await response.json();
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export default function App() {
+  useEffect(() => {
+    console.log("useEffect: ", new Date());
+    // request();
+    // registerBackgroundFetchAsync();
+    getLocationAsync();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Exemplo de Background Fetch com Expo!</Text>
     </View>
   );
 }
@@ -13,8 +78,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
